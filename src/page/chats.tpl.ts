25fbb6af9fae7094c672c55113c7778bfdfcd2d5
Benjamin <sihooleebd@gmail.com>
2021-12-27 19:53:21 +0900

    NEW CHAT ROOMS PAGE

---
+++ src/page/chats.tpl.ts
@@ -0,0 +1,29 @@
+const chatTemplate = `
+  <li data-counterpart-id='{{counterId}}'>
+    <div class='chat-metadata-wrapper'> 
+      <div class='profile'><img src='{{profileImg}}' ></div>
+      <div class='counterpart'>{{counterNickname}}</div>
+    </div>
+    </li>
+`;
+
+const chatsTemplate  = `
+  <ul class='chats'>{{chatsList}}</ul>
+`
+
+const template = `
+  <div class='chats page with-title'>
+    <div class='title'>
+      <img src='/dist-static/logo.png'>
+      <div class='nav-button-wrapper right'>
+      </div>
+      <div class='nav-button-wrapper left'>
+        <input type='button' class='nav-button' id='menu' value='menu'>
+      </div>
+    </div>
+    <div class='main-header'>All Chats</div>
+    {{chatsList}}
+  </div>
+`;
+export { chatTemplate, chatsTemplate };
+export default template;
