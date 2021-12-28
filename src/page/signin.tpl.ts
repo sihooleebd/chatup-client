a4bacb3dd58c457dbed7c260b318e90e8cb9796a
Benjamin <sihooleebd@gmail.com>
2021-12-28 23:57:09 +0900

    다시 원래대로....

--- src/page/signin.tpl.ts
+++ src/page/signin.tpl.ts
@@ -11,7 +11,7 @@ const template = `
           <input type='password' placeholder='password' id='pw'>
         </li>
       </ul>
-      <input type='submit' class='hoverable-button' value="log in">
+      <input type='submit' value="log in">
       <br>
       <input type='button' id='btn-goto-signup' value="don't have an account? sign up">
     </form>
