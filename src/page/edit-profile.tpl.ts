069ae2e5bc844d6793cda7dbfbc51b962e42bf92
Benjamin <sihooleebd@gmail.com>
2022-01-05 15:17:43 +0900

    ㅁㄴㄹㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

--- src/page/edit-profile.tpl.ts
+++ src/page/edit-profile.tpl.ts
@@ -1,7 +1,7 @@
 const template = `
   <div class='edit-profile page'>
     <form autocomplete="off">
-      <div class='guide'><b>Welcome to the dressing room!</b><br>Change your look!</div>
+      <div class='guide'><b>Welcome!</b><br>Change your look!</div>
       <div class='photo-wrapper'> 
         <div class='profile-viewer'></div>
         <input type='file' name='photo' id='photo' accept='image/jpg, image/png'/>
